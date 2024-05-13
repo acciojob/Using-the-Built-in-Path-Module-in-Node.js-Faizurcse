716a098d6a07068eeb0ba39e16168ef9fdae2d19
Faizurcse <mdfaizurrahman2000@gmail.com>
2024-05-14 02:02:50 +0530

    heyfaiznode

--- main.js
+++ main.js
@@ -1,22 +1,10 @@
-// Import the path module
-const path = require('path');
+// main.js
+const path = require("path");
 
-// Function to join two file path segments
-function joinPaths(segment1, segment2) {
-    // Join the two segments using the path module
-    const joinedPath = path.join(segment1, segment2);
-    return joinedPath;
+function joinPaths(input1, input2) {
+  const joinedPath = path.join(input1, input2);
+  console.log(joinedPath);
 }
 
-// Accept two command-line arguments
-const segment1 = process.argv[2];
-const segment2 = process.argv[3];
-
-// Check if both command-line arguments are provided
-if (segment1 && segment2) {
-    // Call the joinPaths function with the provided segments
-    const result = joinPaths(segment1, segment2);
-    console.log('Joined Path:', result);
-} else {
-    console.log('Please provide two file path segments.');
-}
+const args = process.argv.slice(2);
+joinPaths(args[0], args[1]);
